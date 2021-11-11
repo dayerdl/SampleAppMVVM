79fe338fcd4238ff06f28e19816f991dda8a6c7c
David Lopez Dayer <davidlopez.dayer@dnata.com>
2021-11-11 16:58:35 +0400

    Retrieve data from API

--- app/src/main/java/com/example/sampleappmvvm/server/Api.kt
+++ app/src/main/java/com/example/sampleappmvvm/server/Api.kt
@@ -1,10 +1,16 @@
 package com.example.sampleappmvvm.server
 
 import com.example.sampleappmvvm.BuildConfig
+import io.reactivex.Observable
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface Api {
-    @GET(BuildConfig.BASE_URL)
-    fun loadData(): Single<ServerResponse>
+
+    @GET("movie/{movie_id}")
+    fun loadData(@Path("movie_id")movieId: Int = 19404,
+                        @Query("api_key") apiKey: String = "07473a01a734d6aa462ef4b401276805"
+    ): Single<Movie>
 }
